c65809600e3559600925ba9868bfc9e758da2817
luis <luisfgp80@gmail.com>
2022-09-15 17:18:32 -0400

    update

--- algoexpert/tournamentWinner.js
+++ algoexpert/tournamentWinner.js
@@ -3,27 +3,27 @@
 //Sedond array is the results of all the matches where 1 represents home won and 0 is away won
 //Write a function to determine who won the tournament
 
-
 const competitions = [
-    ["HTML", "C#"],
-    ["C#", "Python"],
-    ["Python", "HTML"]
-]
+  ["HTML", "C#"],
+  ["C#", "Python"],
+  ["Python", "HTML"],
+];
 
-const results = [0,0,1]
+const results = [0, 0, 1];
 
 function tournamentWinner(competitions, results) {
-    let teamsScores = {"":0}
-    let winner = '';
-
-    for (i in results) {
-        if (results[i] === 1) {
-            if (!competitions[i][0] in teamsScores) {
-                teamsScores[competitions[i][0] = 0];
-            }
-        }
+  let teams = {};
+  for (i in competitions) {
+    teams[competitions[i][0]] = 0;
+  }
+  for (i in results) {
+    if (results[i] === 0) {
+      teams[competitions[i][1]] += 3;
+    } else {
+      teams[competitions[i][0]] += 3;
     }
-    return teamsScores;
+  }
+  return teams.;
 }
 
 console.log(tournamentWinner(competitions, results));
